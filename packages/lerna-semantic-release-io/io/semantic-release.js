b122378386a4703db336ce1c8158c13df5b8b10a
Vojtech Toman <cry.wolf@centrum.cz>
2017-01-20 17:53:22 +0100

    feat: Use 'tag-summary' package instead of '@semantic-release/last-release-npm'
    Commits range is assessed from tags instead of npm versions
    
    affects: lerna-semantic-release, lerna-semantic-release-io

--- packages/lerna-semantic-release-io/io/semantic-release.js
+++ packages/lerna-semantic-release-io/io/semantic-release.js
@@ -3,7 +3,7 @@ var srNormalize = require('semantic-release/src/lib/plugins').normalize;
 var verifyRelease = srNormalize({}, "semantic-release/src/lib/plugin-noop");
 var analyzeCommits = require('lerna-semantic-release-analyze-commits').analyze;
 var getLastRelease = require('lerna-semantic-release-get-last-release').bind(null, {
-  lastReleaseNpm: require('@semantic-release/last-release-npm'),
+  lastReleaseNpm: require('tag-summary').get,
   revParse: require('./git').revParse(),
   tagList: require('./git').tagList(),
 });
